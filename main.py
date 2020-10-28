767bc70ca64becca071a7556e898524f89d22091
Tyler <35055803+trstamp@users.noreply.github.com>
2020-10-28 00:59:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,56 @@ import networkx
 import gzip
 import matplotlib.pyplot as plt
 
-def main():
+def degAndCope():
+    # Read file
+    file = gzip.open('twitter_combined.txt.gz')
+
+    Graphtype = networkx.DiGraph()
+    graph = networkx.read_edgelist(file, create_using=Graphtype, nodetype=int, data=('weight', float),)
+
+    
+    # Degree Ratio
+    degratio = []
+    print("Degree Ratios")
+    for item in graph.nodes():
+        # Nodes with no cycles go into the if
+        if graph.in_degree(item) == 0:
+            degratio.append(0.0)
+        else:
+            degratio.append(graph.out_degree(item) / graph.in_degree(item))
+
+    plt.title("Degree Ratio")
+    plt.hist(degratio, bins=6)
+    plt.xlim(0, 60)
+    plt.show()
+
+    # CopelandScore
+    copeland = []
+    print("Copeland Scores")
+    for item in graph.nodes():
+        copeland.append(graph.out_degree(item) - graph.in_degree(item))
+
+    plt.title("Copeland Scores")
+    plt.hist(copeland, bins=15)
+    plt.xlim(-200, 200)
+    plt.show()
+
+    file.close()
+
+def centralities():
     # Read file
     file = gzip.open('twitter_combined.txt.gz')
     Graphtype = networkx.DiGraph()
     graph = networkx.read_edgelist(file, create_using=Graphtype, nodetype=int, data=('weight', float), )
 
-    # Degree Ratio
-    print("Degree Ratios")
-    for item in graph.nodes():
-        # Nodes with no cycles go into the if
-        if graph.in_degree(item) == 0:
-            degratio = 0.0
-        else:
-            degratio = graph.out_degree(item) / graph.in_degree(item)
-
-    # CopelandScore
-    print("Copeland Scores")
-    for item in graph.nodes():
-        print("Node ", item, ": ", (graph.out_degree(item) - graph.in_degree(item)))
-
     # Closeness Centrality
     closeness = sorted(networkx.closeness_centrality(graph).values(), reverse=True)
-    #for item in graph.nodes():
+    # for item in graph.nodes():
     #    networkx.closeness_centrality(graph, item)
-    plt.loglog(closeness)
+    plt.hist(x=closeness)
     plt.title("Closeness Centrality")
     plt.xlabel(" ")
     plt.ylabel(" ")
-
 
     # Betweeness Centrality
     networkx.betweenness_centrality(graph)
@@ -38,6 +59,4 @@ def main():
     # Triadic Census
     print(networkx.triadic_census(graph))
 
-    file.close()
-
-main()
+degAndCope()
