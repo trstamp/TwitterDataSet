08f164db972814b690ee2ccab0a11efd7ee31b2c
Tyler <35055803+trstamp@users.noreply.github.com>
2020-10-29 00:03:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import networkx
 import gzip
 import matplotlib.pyplot as plt
+import statistics
+import numpy
 
 def degAndCope():
     # Read file
@@ -19,18 +21,25 @@ def degAndCope():
         else:
             degratio.append(graph.out_degree(item) / graph.in_degree(item))
 
+    degratio.sort()
+    print("Mean: ", sum(degratio)/len(degratio))
+    print("Median: ", statistics.median(degratio))
+    print("Standard Deviation: ", numpy.std(degratio))
     plt.title("Degree Ratio")
     plt.xlabel(" Degree Ratio Value ")
     plt.ylabel(" Num of nodes ")
-    plt.hist(degratio, bins=120)
-    plt.xlim(0, 60)
+    plt.hist(degratio, bins=1000)
+    plt.xlim(0, 20)
     plt.show()
 
-    # CopelandScore
+    # Copeland Score
     copeland = []
     for item in graph.nodes():
         copeland.append(graph.out_degree(item) - graph.in_degree(item))
 
+    print("Mean: ", sum(copeland)/len(copeland))
+    print("Median: ", statistics.median(degratio))
+    print("Standard Deviation: ", numpy.std(copeland))
     plt.title("Copeland Scores")
     plt.xlabel(" Copeland Score Value ")
     plt.ylabel(" Num of nodes ")
@@ -40,29 +49,38 @@ def degAndCope():
 
     file.close()
 
-def centralities():
+def cCentrality():
     # Read file
     file = gzip.open('twitter_combined.txt.gz')
     Graphtype = networkx.DiGraph()
     graph = networkx.read_edgelist(file, create_using=Graphtype, nodetype=int, data=('weight', float), )
 
     # Closeness Centrality
-    #closeness = sorted(networkx.closeness_centrality(graph).values(), reverse=True)
     closeness = []
     for item in graph.nodes():
         close = networkx.closeness_centrality(graph, item)
+        print(close)
         closeness.append(close)
     plt.hist(closeness, bins=50)
+    plt.xlim(0, 1)
     plt.title("Closeness Centrality")
     plt.xlabel(" Closeness Centrality Value ")
     plt.ylabel(" Num of nodes ")
     plt.show()
 
+    file.close()
+
+def bCentrality():
+    # Read file
+    file = gzip.open('twitter_combined.txt.gz')
+    Graphtype = networkx.DiGraph()
+    graph = networkx.read_edgelist(file, create_using=Graphtype, nodetype=int, data=('weight', float), )
+
     # Betweeness Centrality
-    betweeness = []
-    for item in graph.nodes():
-        between = networkx.betweenness_centrality(graph, item)
-        betweeness.append(between)
+    betweeness = networkx.betweenness_centrality(graph)
+    print("Median: ", statistics.median(betweeness))
+    print("Mean: ", statistics.mean(betweeness))
+    print("Standard Deviation: ", numpy.std(betweeness))
     plt.hist(betweeness, bins=50)
     plt.title("Betweeness Centrality")
     plt.xlabel(" Betweeness Centrality Value ")
@@ -70,6 +88,7 @@ def centralities():
     plt.show()
 
     file.close()
+
 
 def triads():
     # Read file
@@ -79,12 +98,11 @@ def triads():
 
     # Triadic Census
     triadic = networkx.triadic_census(graph)
-    
     print(triadic)
 
     file.close()
 
-
 degAndCope()
-centralities()
+cCentrality()
+bCentrality()
 triads()
